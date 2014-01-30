456aa3cfde044280040e4cb9a48c743a44c48077
Keith Morgado <keith.morgado@gmail.com>
2014-01-29 22:51:18 -0500

    First Big commit, Things work

---
+++ Unity 4.3/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		//Screen.SetResolution(600,1240,true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Login_BtnClick()
+	{
+		Application.LoadLevel(1);
+	}
+}
